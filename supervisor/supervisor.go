22d4342e71ac2e00c1d5c9c8fc340465a37242bb
matteo fernandes <mf7compte@gmail.com>
2026-02-22 23:38:32 +0100

    add startOnly

--- supervisor/supervisor.go
+++ supervisor/supervisor.go
@@ -11,23 +11,21 @@ import (
 )
 
 type Supervisor struct {
-	Prs map[string]*process.Process
-	cfg *config.Config
+	Prs     map[string]*process.Process
+	cfg     *config.Config
 	CfgPath string
 }
 
-
-
 func New(cfg *config.Config, path string) *Supervisor {
-    return &Supervisor{Prs: make(map[string]*process.Process), cfg: cfg, CfgPath: path}
+	return &Supervisor{Prs: make(map[string]*process.Process), cfg: cfg, CfgPath: path}
 }
 
-func (s *Supervisor) Start() error{
-	for name, prog := range(s.cfg.Programs){
+func (s *Supervisor) Start() error {
+	for name, prog := range s.cfg.Programs {
 		s.Prs[name] = process.New(prog.Cmd, name)
-		if prog.AutoStart{
+		if prog.AutoStart {
 			err := s.Prs[name].Start()
-			if err != nil{
+			if err != nil {
 				return err
 			}
 			go s.Watch(name)
@@ -36,25 +34,24 @@ func (s *Supervisor) Start() error{
 	return nil
 }
 
-
 func (s *Supervisor) Status() {
-    for name, p := range s.Prs {
-        fmt.Printf("%-20s %-10s pid %d\n", name, p.GetState(), p.Pid())
-    }
+	for name, p := range s.Prs {
+		fmt.Printf("%-20s %-10s pid %d\n", name, p.GetState(), p.Pid())
+	}
 }
 
-func (s *Supervisor) StartJob(name string) error{
+func (s *Supervisor) StartJob(name string) error {
 
 	if _, ok := s.Prs[name]; !ok {
-    	fmt.Printf("unknown program: %s\n", name)
-    	return nil
+		fmt.Printf("unknown program: %s\n", name)
+		return nil
 	}
-	if s.Prs[name].GetState() == process.Running{
+	if s.Prs[name].GetState() == process.Running {
 		fmt.Printf("Process already running")
 		return nil
 	}
 	err := s.Prs[name].Start()
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	go s.Watch(name)
@@ -62,93 +59,98 @@ func (s *Supervisor) StartJob(name string) error{
 
 }
 
-func (s *Supervisor) StopJob(name string) error{
+func (s *Supervisor) StopJob(name string) error {
 
 	if _, ok := s.Prs[name]; !ok {
-    	fmt.Printf("unknown program: %s\n", name)
-    	return nil
+		fmt.Printf("unknown program: %s\n", name)
+		return nil
 	}
-	if s.Prs[name].GetState() == process.Stopped{
+	if s.Prs[name].GetState() == process.Stopped {
 		fmt.Printf("Process already stopped")
 		return nil
 	}
 	err := s.Prs[name].Stop()
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	return nil
 
 }
 
-
-func (s *Supervisor) RestartJob(name string) error{
+func (s *Supervisor) RestartJob(name string) error {
+	select {
+	case <-s.Prs[name].Done:
+	default:
+	}
 	if _, ok := s.Prs[name]; !ok {
 		fmt.Printf("Unknown program: %s\n", name)
 		return nil
 	}
 	err := s.Prs[name].Stop()
 	if err != nil {
-    	logger.Log(fmt.Sprintf("error stopping %s: %v", name, err))
+		logger.Log(fmt.Sprintf("error stopping %s: %v", name, err))
 	}
+	<-s.Prs[name].Done
 	err = s.Prs[name].Start()
-	if err != nil{
+	if err != nil {
 		return err
 	}
-	s.Watch(name)
 	return nil
 }
-
 
 func (s *Supervisor) Reload(newCfg *config.Config) {
 	var flag int
 
 	for name, prog := range newCfg.Programs {
 		flag = 0
-		for curr_name, curr_prog := range s.cfg.Programs{
-			if curr_name == name{
+		for curr_name, curr_prog := range s.cfg.Programs {
+			if curr_name == name {
 				flag = 1
-				if reflect.DeepEqual(curr_prog, prog){ // ==
+				if reflect.DeepEqual(curr_prog, prog) { // ==
 					break
 				} else {
 					s.RestartJob(name)
 				}
 			}
 		}
-		if flag == 0{
+		if flag == 0 {
 			s.Prs[name] = process.New(prog.Cmd, name)
 
 			if prog.AutoStart {
-        		s.StartJob(name)
-	    	}
+				s.StartJob(name)
+			}
 		}
 	}
 
 	for name, p := range s.Prs {
-    	if _, exists := newCfg.Programs[name]; !exists {
-        	p.Stop()
-        	delete(s.Prs, name)
-    	}
+		if _, exists := newCfg.Programs[name]; !exists {
+			p.Stop()
+			delete(s.Prs, name)
+		}
 	}
-    s.cfg = newCfg
+	s.cfg = newCfg
 }
-
 
 func (s *Supervisor) Watch(name string) {
 	s.Prs[name].Wait()
-    err := <-s.Prs[name].Done
-    prog := s.cfg.Programs[name]
-    
-    switch prog.AutoRestart {
-    case "always":
+	err := <-s.Prs[name].Done
+	prog := s.cfg.Programs[name]
+
+	switch prog.AutoRestart {
+	case "always":
 		logger.LogRestart(name)
-        s.StartJob(name)
-        s.Watch(name)
-    case "unexpected":
-        if err != nil {
+		s.startOnly(name)
+		s.Watch(name)
+	case "unexpected":
+		if err != nil {
 			logger.LogRestart(name)
-            s.StartJob(name)
-        	s.Watch(name)
-        }
-    case "never":
-    }
+			s.startOnly(name)
+			s.Watch(name)
+		}
+	case "never":
+	}
+}
+
+func (s *Supervisor) startOnly(name string) error {
+	return s.Prs[name].Start()
 }
